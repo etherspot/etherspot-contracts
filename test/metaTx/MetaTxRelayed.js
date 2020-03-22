cbf5fd9f26bb45e907ff82abc7acdbc31356643b
Stanisław Głogowski <stanislaw.glogowski@gmail.com>
2020-03-22 14:01:36 +0100

    test(metaTx): flat tests

--- test/metaTx/MetaTxRelayed.js
+++ test/metaTx/MetaTxRelayed.js
@@ -28,27 +28,25 @@ contract('MetaTxRelayed (using mock)', (addresses) => {
     });
   });
 
-  context('scenarios', () => {
-    describe('without relayer', () => {
-      it('expect to call example #1', async () => {
-        const output = await relayedMock.exampleCall1();
+  context('calls without relayer', () => {
+    it('expect to call example #1', async () => {
+      const output = await relayedMock.exampleCall1();
 
-        logGasUsed(output);
+      logGasUsed(output);
 
-        const { logs: [log] } = output;
+      const { logs: [log] } = output;
 
-        expect(log.args.sender).toBe(from);
-      });
+      expect(log.args.sender).toBe(from);
+    });
 
-      it('expect to call example #2', async () => {
-        const output = await relayedMock.exampleCall2(1);
+    it('expect to call example #2', async () => {
+      const output = await relayedMock.exampleCall2(1);
 
-        logGasUsed(output);
+      logGasUsed(output);
 
-        const { logs: [log] } = output;
+      const { logs: [log] } = output;
 
-        expect(log.args.sender).toBe(from);
-      });
+      expect(log.args.sender).toBe(from);
     });
   });
 });
