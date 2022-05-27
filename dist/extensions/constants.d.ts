ffee15cf8a2f87401883ffe177d00b3102c092b2
zincoshine <psramanuj@gmail.com>
2022-05-27 10:21:38 +0100

    Updated the contract addresses

--- dist/extensions/constants.d.ts
+++ dist/extensions/constants.d.ts
@@ -22,6 +22,8 @@ export declare enum NetworkNames {
     OptimismKovan = "optimismKovan",
     Moonbeam = "moonbeam",
     Moonbase = "moonbase",
+    Celo = "celo",
+    CeloTest = "celoTest",
     Etherspot = "etherspot",
     LocalA = "localA",
     LocalB = "localB",
