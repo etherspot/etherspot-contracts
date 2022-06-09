e21c16af1a19219e16847e66bc533bc2effb2e27
Luke Wickens <71776468+lukewickens1989@users.noreply.github.com>
2022-06-09 14:43:54 +0100

    deployment script and solidity-coverage upgrade
    Added deployment script for BalancesHelperV2 to deploy/001_create-common, upgraded solidity-coverage to latest version to provide support for new solidity code.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,18 +1,18 @@
-import '@nomiclabs/hardhat-etherscan';
+import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-web3";
-import '@typechain/hardhat';
-import 'hardhat-deploy';
-import 'hardhat-deploy-ethers';
-import 'hardhat-gas-reporter';
-import 'solidity-coverage';
-import { HardhatUserConfig } from 'hardhat/config';
-import { utils } from 'ethers';
+import "@typechain/hardhat";
+import "hardhat-deploy";
+import "hardhat-deploy-ethers";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+import { HardhatUserConfig } from "hardhat/config";
+import { utils } from "ethers";
 import {
   NetworkNames,
   ContractNames,
   createConfigNetworks,
   NETWORK_CONFIGS,
-} from './extensions';
+} from "./extensions";
 
 const { HARDHAT_MNEMONIC, ETHERSCAN_API_KEY } = process.env;
 
@@ -25,7 +25,7 @@ const config: HardhatUserConfig = {
       accounts: {
         mnemonic:
           HARDHAT_MNEMONIC ||
-          'test test test test test test test test test test test junk',
+          "test test test test test test test test test test test junk",
         count: 256,
       },
       chainId: 3333,
@@ -36,72 +36,72 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-      version: '0.6.12',
-      settings: {
-        evmVersion: 'istanbul',
-        metadata: {
-          bytecodeHash: 'none',
+        version: "0.6.12",
+        settings: {
+          evmVersion: "istanbul",
+          metadata: {
+            bytecodeHash: "none",
+          },
         },
       },
-    },
-    {
-      version: '0.8.4',
+      {
+        version: "0.8.4",
         settings: {
-          evmVersion: 'istanbul',
+          evmVersion: "istanbul",
           metadata: {
-            bytecodeHash: 'none',
+            bytecodeHash: "none",
           },
         },
       },
     ],
   },
   paths: {
-    sources: 'src',
-    cache: '.hardhat/cache',
-    artifacts: '.hardhat/artifacts',
-    deploy: 'deploy',
-    deployments: 'deployments',
+    sources: "src",
+    cache: ".hardhat/cache",
+    artifacts: ".hardhat/artifacts",
+    deploy: "deploy",
+    deployments: "deployments",
   },
   buildPaths: {
-    artifacts: 'artifacts',
-    dist: 'dist',
+    artifacts: "artifacts",
+    dist: "dist",
   },
   typechain: {
-    outDir: 'typings',
-    target: 'ethers-v5',
+    outDir: "typings",
+    target: "ethers-v5",
   },
   deterministicDeployment: {
     [NETWORK_CONFIGS[NetworkNames.Fuji].chainId]: {
-      factory: '0x914d7Fec6aaC8cd542e72Bca78B30650d45643d7',
-      deployer: '0xE1CB04A0fA36DdD16a06ea828007E35e1a3cBC37',
-      funding: '10000000000000000',
+      factory: "0x914d7Fec6aaC8cd542e72Bca78B30650d45643d7",
+      deployer: "0xE1CB04A0fA36DdD16a06ea828007E35e1a3cBC37",
+      funding: "10000000000000000",
       signedTx:
-        '0xf8a88085174876e800830186a08080b853604580600e600039806000f350fe7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe03601600081602082378035828234f58015156039578182fd5b8082525050506014600cf3830150f6a032b2a806bfc34024c6638848c3798213261304af82de14002ca2b4961a643b95a03c74c13eda5ff6b9b821fbccd1a67f160eb6a0ca50dad04b7a3e564e2599722e',
+        "0xf8a88085174876e800830186a08080b853604580600e600039806000f350fe7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe03601600081602082378035828234f58015156039578182fd5b8082525050506014600cf3830150f6a032b2a806bfc34024c6638848c3798213261304af82de14002ca2b4961a643b95a03c74c13eda5ff6b9b821fbccd1a67f160eb6a0ca50dad04b7a3e564e2599722e",
     },
     [NETWORK_CONFIGS[NetworkNames.Avalanche].chainId]: {
-      factory: '0x914d7Fec6aaC8cd542e72Bca78B30650d45643d7',
-      deployer: '0xE1CB04A0fA36DdD16a06ea828007E35e1a3cBC37',
-      funding: '10000000000000000',
+      factory: "0x914d7Fec6aaC8cd542e72Bca78B30650d45643d7",
+      deployer: "0xE1CB04A0fA36DdD16a06ea828007E35e1a3cBC37",
+      funding: "10000000000000000",
       signedTx:
-        '0xf8a88085174876e800830186a08080b853604580600e600039806000f350fe7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe03601600081602082378035828234f58015156039578182fd5b8082525050506014600cf3830150f8a0d69f418f6da8f01fb95d6d87e1f2eabd85884784cd2ba2d306ba066d41b7c5e6a05a2b76982a148ca8ca2803ceac3d39a3f26208b654c473b17b01e7536eeba55e',
+        "0xf8a88085174876e800830186a08080b853604580600e600039806000f350fe7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe03601600081602082378035828234f58015156039578182fd5b8082525050506014600cf3830150f8a0d69f418f6da8f01fb95d6d87e1f2eabd85884784cd2ba2d306ba066d41b7c5e6a05a2b76982a148ca8ca2803ceac3d39a3f26208b654c473b17b01e7536eeba55e",
     },
   },
   gasReporter: {
     enabled: false,
   },
   ens: {
-    internalTopLevelDomains: ['pillar', 'etherspot', 'dank'],
+    internalTopLevelDomains: ["pillar", "etherspot", "dank"],
   },
   knownContracts: {
     [NetworkNames.Mainnet]: {
-      [ContractNames.ENSRegistry]: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
+      [ContractNames.ENSRegistry]: "0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e",
     },
   },
   create2Salts: {
-    default: utils.id('ETHERspot'),
-    [ContractNames.ENSHelper]: utils.id('ETHERspot@1.2.0'),
-    [ContractNames.ENSRegistry]: utils.id('ETHERspot@1.2.0'),
-    [ContractNames.ENSReverseRegistrar]: utils.id('ETHERspot@1.2.0'),
+    default: utils.id("ETHERspot"),
+    [ContractNames.ENSHelper]: utils.id("ETHERspot@1.2.0"),
+    [ContractNames.ENSRegistry]: utils.id("ETHERspot@1.2.0"),
+    [ContractNames.ENSReverseRegistrar]: utils.id("ETHERspot@1.2.0"),
   },
   etherscan: {
     apiKey: ETHERSCAN_API_KEY,
--- test/common/helpers/BalancesHelperV2.ts
+++ test/common/helpers/BalancesHelperV2.ts
@@ -1,14 +1,14 @@
-import { BigNumber } from 'ethers';
-import { deployContract } from '../../shared';
-import { BalancesHelperV2 } from '../../../typings';
-import { Framework } from '@superfluid-finance/sdk-core';
-import daiABI from './abi/fDAIABI';
-import { ethers, web3 } from 'hardhat';
-import { SignerWithAddress } from 'hardhat-deploy-ethers/dist/src/signers';
-import { expectRevert } from '@openzeppelin/test-helpers';
-import deployFramework from '@superfluid-finance/ethereum-contracts/scripts/deploy-framework';
-import deployTestToken from '@superfluid-finance/ethereum-contracts/scripts/deploy-test-token';
-import deploySuperToken from '@superfluid-finance/ethereum-contracts/scripts/deploy-super-token';
+import { BigNumber } from "ethers";
+import { deployContract } from "../../shared";
+import { BalancesHelperV2 } from "../../../typings";
+import { Framework } from "@superfluid-finance/sdk-core";
+import daiABI from "./abi/fDAIABI";
+import { ethers, web3 } from "hardhat";
+import { SignerWithAddress } from "hardhat-deploy-ethers/dist/src/signers";
+import { expectRevert } from "@openzeppelin/test-helpers";
+import deployFramework from "@superfluid-finance/ethereum-contracts/scripts/deploy-framework";
+import deployTestToken from "@superfluid-finance/ethereum-contracts/scripts/deploy-test-token";
+import deploySuperToken from "@superfluid-finance/ethereum-contracts/scripts/deploy-super-token";
 
 const provider = web3;
 
@@ -27,12 +27,12 @@ const errorHandler = (err: any) => {
   if (err) throw err;
 };
 
-describe('BalancesHelperV2', () => {
+describe("BalancesHelperV2", () => {
   before(async () => {
     // create signers
     [owner, alice, bob, charlie, dead] = await ethers.getSigners();
     // deploy BalancesHelperV2 contract
-    balancesHelperV2 = await deployContract('BalancesHelperV2');
+    balancesHelperV2 = await deployContract("BalancesHelperV2");
     // deploy the framework
     await deployFramework(errorHandler, { web3, from: owner.address });
     // deploy a fake erc20
@@ -40,32 +40,32 @@ describe('BalancesHelperV2', () => {
 
   beforeEach(async () => {
     // deploy a fake erc20
-    await deployTestToken(errorHandler, [':', 'fDAI'], {
+    await deployTestToken(errorHandler, [":", "fDAI"], {
       web3,
       from: owner.address,
     });
     // deploy a fake erc20 wrapper super token around the fDAI token
-    await deploySuperToken(errorHandler, [':', 'fDAI'], {
+    await deploySuperToken(errorHandler, [":", "fDAI"], {
       web3,
       from: owner.address,
     });
     // initialize the superfluid framework - networkName: 'custom' and dataMode: 'WEB3_ONLY' as using hardhat locally
     sf = await Framework.create({
-      networkName: 'custom',
+      networkName: "custom",
       provider,
-      dataMode: 'WEB3_ONLY',
+      dataMode: "WEB3_ONLY",
       resolverAddress: process.env.RESOLVER_ADDRESS,
-      protocolReleaseVersion: 'test',
+      protocolReleaseVersion: "test",
     });
     // use the framework to get the super token
-    daix = await sf.loadSuperToken('fDAIx');
+    daix = await sf.loadSuperToken("fDAIx");
     // get the contract object for the erc20 token
     const daiAddress = daix.underlyingToken.address;
     dai = new ethers.Contract(daiAddress, daiABI, owner);
   });
 
-  describe('Pure SuperToken', () => {
-    it('Should be able to read user balance', async () => {
+  describe("Pure SuperToken", () => {
+    it("Should be able to read user balance", async () => {
       daiAddress = daix.underlyingToken.address;
       // check fDAI balance (should be 0)
       let balance: BigNumber[] = await balancesHelperV2.getSuperfluidPureTokenBalances(
@@ -76,20 +76,20 @@ describe('BalancesHelperV2', () => {
       // mint user tokens
       await dai
         .connect(owner)
-        .mint(owner.address, ethers.utils.parseEther('100'));
+        .mint(owner.address, ethers.utils.parseEther("100"));
       // check fDAI balance (should be 100)
       balance = await balancesHelperV2.getSuperfluidPureTokenBalances(
         [owner.address],
         [daiAddress],
       );
-      expect(balance[0]).toBeBN(ethers.utils.parseEther('100'));
+      expect(balance[0]).toBeBN(ethers.utils.parseEther("100"));
       // remove tokens
       await dai
         .connect(owner)
-        .transfer(dead.address, ethers.utils.parseEther('100'));
+        .transfer(dead.address, ethers.utils.parseEther("100"));
     });
 
-    it('Should be able to read multiple user balances', async () => {
+    it("Should be able to read multiple user balances", async () => {
       daiAddress = daix.underlyingToken.address;
       // check fDAI balances (should all be 0)
       let balance: BigNumber[] = await balancesHelperV2.getSuperfluidPureTokenBalances(
@@ -103,39 +103,39 @@ describe('BalancesHelperV2', () => {
       // mint users tokens
       await dai
         .connect(owner)
-        .mint(owner.address, ethers.utils.parseEther('100'));
+        .mint(owner.address, ethers.utils.parseEther("100"));
       await dai
         .connect(owner)
-        .mint(alice.address, ethers.utils.parseEther('10'));
-      await dai.connect(owner).mint(bob.address, ethers.utils.parseEther('20'));
+        .mint(alice.address, ethers.utils.parseEther("10"));
+      await dai.connect(owner).mint(bob.address, ethers.utils.parseEther("20"));
       await dai
         .connect(owner)
-        .mint(charlie.address, ethers.utils.parseEther('30'));
+        .mint(charlie.address, ethers.utils.parseEther("30"));
       // check fDAI balances (should be 100, 10, 20, 30)
       balance = await balancesHelperV2.getSuperfluidPureTokenBalances(
         [owner.address, alice.address, bob.address, charlie.address],
         [daiAddress],
       );
-      expect(balance[0]).toBeBN(ethers.utils.parseEther('100'));
-      expect(balance[1]).toBeBN(ethers.utils.parseEther('10'));
-      expect(balance[2]).toBeBN(ethers.utils.parseEther('20'));
-      expect(balance[3]).toBeBN(ethers.utils.parseEther('30'));
+      expect(balance[0]).toBeBN(ethers.utils.parseEther("100"));
+      expect(balance[1]).toBeBN(ethers.utils.parseEther("10"));
+      expect(balance[2]).toBeBN(ethers.utils.parseEther("20"));
+      expect(balance[3]).toBeBN(ethers.utils.parseEther("30"));
       // remove tokens
       await dai
         .connect(owner)
-        .transfer(dead.address, ethers.utils.parseEther('100'));
+        .transfer(dead.address, ethers.utils.parseEther("100"));
       await dai
         .connect(alice)
-        .transfer(dead.address, ethers.utils.parseEther('10'));
+        .transfer(dead.address, ethers.utils.parseEther("10"));
       await dai
         .connect(bob)
-        .transfer(dead.address, ethers.utils.parseEther('20'));
+        .transfer(dead.address, ethers.utils.parseEther("20"));
       await dai
         .connect(charlie)
-        .transfer(dead.address, ethers.utils.parseEther('30'));
+        .transfer(dead.address, ethers.utils.parseEther("30"));
     });
 
-    it('Should revert if the account address is the zero address', async () => {
+    it("Should revert if the account address is the zero address", async () => {
       daiAddress = daix.underlyingToken.address;
       await expectRevert(
         balancesHelperV2.getSuperfluidPureTokenBalances(
@@ -146,7 +146,7 @@ describe('BalancesHelperV2', () => {
       );
     });
 
-    it('Should revert if the token address is the zero address', async () => {
+    it("Should revert if the token address is the zero address", async () => {
       await expectRevert(
         balancesHelperV2.getSuperfluidPureTokenBalances(
           [owner.address],
@@ -157,16 +157,16 @@ describe('BalancesHelperV2', () => {
     });
   });
 
-  describe('Wrapped/Native SuperToken', () => {
-    it('Should be able to read user balance (upgraded tokens)', async () => {
+  describe("Wrapped/Native SuperToken", () => {
+    it("Should be able to read user balance (upgraded tokens)", async () => {
       daiAddress = daix.underlyingToken.address;
       // mint & approve contract to spend user tokens
       await dai
         .connect(owner)
-        .mint(owner.address, ethers.utils.parseEther('100'));
+        .mint(owner.address, ethers.utils.parseEther("100"));
       await dai
         .connect(owner)
-        .approve(daix.address, ethers.utils.parseEther('50'));
+        .approve(daix.address, ethers.utils.parseEther("50"));
       // check fDAIx initial balance is zero
       let balance: BigNumber[] = await balancesHelperV2.getSuperfluidWrappedTokenBalances(
         [owner.address],
@@ -175,7 +175,7 @@ describe('BalancesHelperV2', () => {
       expect(balance[0]).toBeBN(0);
       // upgrade half balance (50) to SuperToken
       const daixUpgradeOperation = daix.upgrade({
-        amount: ethers.utils.parseEther('50'),
+        amount: ethers.utils.parseEther("50"),
       });
       await daixUpgradeOperation.exec(owner);
       // check fDAIx balance (should be 50)
@@ -183,34 +183,34 @@ describe('BalancesHelperV2', () => {
         [owner.address],
         [daix.address],
       );
-      expect(balance[0]).toBeBN(ethers.utils.parseEther('50'));
+      expect(balance[0]).toBeBN(ethers.utils.parseEther("50"));
       // check fDAI balance (should be 50)
       balance = await balancesHelperV2.getSuperfluidPureTokenBalances(
         [owner.address],
         [daiAddress],
       );
-      expect(balance[0]).toBeBN(ethers.utils.parseEther('50'));
+      expect(balance[0]).toBeBN(ethers.utils.parseEther("50"));
       // remove wrapped tokens
       const daixDowngradeOperation = daix.downgrade({
-        amount: ethers.utils.parseEther('50'),
+        amount: ethers.utils.parseEther("50"),
       });
       await daixDowngradeOperation.exec(owner);
       await dai
         .connect(owner)
-        .transfer(dead.address, ethers.utils.parseEther('100'));
+        .transfer(dead.address, ethers.utils.parseEther("100"));
     });
 
-    it('Should be able to read user balance (downgraded tokens)', async () => {
+    it("Should be able to read user balance (downgraded tokens)", async () => {
       daiAddress = daix.underlyingToken.address;
       // mint, approve & upgrade
       await dai
         .connect(owner)
-        .mint(owner.address, ethers.utils.parseEther('100'));
+        .mint(owner.address, ethers.utils.parseEther("100"));
       await dai
         .connect(owner)
-        .approve(daix.address, ethers.utils.parseEther('50'));
+        .approve(daix.address, ethers.utils.parseEther("50"));
       const daixUpgradeOperation = daix.upgrade({
-        amount: ethers.utils.parseEther('50'),
+        amount: ethers.utils.parseEther("50"),
       });
       await daixUpgradeOperation.exec(owner);
       // check fDAIx balance (should be 50)
@@ -218,16 +218,16 @@ describe('BalancesHelperV2', () => {
         [owner.address],
         [daix.address],
       );
-      expect(balance[0]).toBeBN(ethers.utils.parseEther('50'));
+      expect(balance[0]).toBeBN(ethers.utils.parseEther("50"));
       // check fDAI balance (should be 50)
       balance = await balancesHelperV2.getSuperfluidPureTokenBalances(
         [owner.address],
         [daiAddress],
       );
-      expect(balance[0]).toBeBN(ethers.utils.parseEther('50'));
+      expect(balance[0]).toBeBN(ethers.utils.parseEther("50"));
       // downgrade half balance (25) from SuperToken
       const daixDowngradeOperation = daix.downgrade({
-        amount: ethers.utils.parseEther('25'),
+        amount: ethers.utils.parseEther("25"),
       });
       await daixDowngradeOperation.exec(owner);
       // check fDAIx balance (should be 25)
@@ -235,45 +235,45 @@ describe('BalancesHelperV2', () => {
         [owner.address],
         [daix.address],
       );
-      expect(balance[0]).toBeBN(ethers.utils.parseEther('25'));
+      expect(balance[0]).toBeBN(ethers.utils.parseEther("25"));
       // check fDAI balance (should be 75)
       balance = await balancesHelperV2.getSuperfluidPureTokenBalances(
         [owner.address],
         [daiAddress],
       );
-      expect(balance[0]).toBeBN(ethers.utils.parseEther('75'));
+      expect(balance[0]).toBeBN(ethers.utils.parseEther("75"));
     });
 
-    it('Should be able to read multiple user balances (upgraded tokens)', async () => {
+    it("Should be able to read multiple user balances (upgraded tokens)", async () => {
       // mint user tokens
       await dai
         .connect(owner)
-        .mint(alice.address, ethers.utils.parseEther('21'));
-      await dai.connect(owner).mint(bob.address, ethers.utils.parseEther('28'));
+        .mint(alice.address, ethers.utils.parseEther("21"));
+      await dai.connect(owner).mint(bob.address, ethers.utils.parseEther("28"));
       await dai
         .connect(owner)
-        .mint(charlie.address, ethers.utils.parseEther('35'));
+        .mint(charlie.address, ethers.utils.parseEther("35"));
       // approve contract to spend user tokens
       await dai
         .connect(alice)
-        .approve(daix.address, ethers.utils.parseEther('7'));
+        .approve(daix.address, ethers.utils.parseEther("7"));
       await dai
         .connect(bob)
-        .approve(daix.address, ethers.utils.parseEther('14'));
+        .approve(daix.address, ethers.utils.parseEther("14"));
       await dai
         .connect(charlie)
-        .approve(daix.address, ethers.utils.parseEther('21'));
+        .approve(daix.address, ethers.utils.parseEther("21"));
       // upgrade 1/3 balances to SuperToken
       const aliceUpgradeOperation = daix.upgrade({
-        amount: ethers.utils.parseEther('7'),
+        amount: ethers.utils.parseEther("7"),
       });
       await aliceUpgradeOperation.exec(alice);
       const bobUpgradeOperation = daix.upgrade({
-        amount: ethers.utils.parseEther('14'),
+        amount: ethers.utils.parseEther("14"),
       });
       await bobUpgradeOperation.exec(bob);
       const charlieUpgradeOperation = daix.upgrade({
-        amount: ethers.utils.parseEther('21'),
+        amount: ethers.utils.parseEther("21"),
       });
       await charlieUpgradeOperation.exec(charlie);
       // check fDAIx balances (should be 7, 14, 21)
@@ -281,20 +281,20 @@ describe('BalancesHelperV2', () => {
         [alice.address, bob.address, charlie.address],
         [daix.address],
       );
-      expect(balance[0]).toBeBN(ethers.utils.parseEther('7'));
-      expect(balance[1]).toBeBN(ethers.utils.parseEther('14'));
-      expect(balance[2]).toBeBN(ethers.utils.parseEther('21'));
+      expect(balance[0]).toBeBN(ethers.utils.parseEther("7"));
+      expect(balance[1]).toBeBN(ethers.utils.parseEther("14"));
+      expect(balance[2]).toBeBN(ethers.utils.parseEther("21"));
       // check fDAI balances (should be 14, 14, 14)
       balance = await balancesHelperV2.getSuperfluidPureTokenBalances(
         [alice.address, bob.address, charlie.address],
         [daiAddress],
       );
-      expect(balance[0]).toBeBN(ethers.utils.parseEther('14'));
-      expect(balance[1]).toBeBN(ethers.utils.parseEther('14'));
-      expect(balance[2]).toBeBN(ethers.utils.parseEther('14'));
+      expect(balance[0]).toBeBN(ethers.utils.parseEther("14"));
+      expect(balance[1]).toBeBN(ethers.utils.parseEther("14"));
+      expect(balance[2]).toBeBN(ethers.utils.parseEther("14"));
     });
 
-    it('Should revert if the account address is the zero address', async () => {
+    it("Should revert if the account address is the zero address", async () => {
       await expectRevert(
         balancesHelperV2.getSuperfluidWrappedTokenBalances(
           [ethers.constants.AddressZero],
@@ -304,7 +304,7 @@ describe('BalancesHelperV2', () => {
       );
     });
 
-    it('Should revert if the token address is the zero address', async () => {
+    it("Should revert if the token address is the zero address", async () => {
       await expectRevert(
         balancesHelperV2.getSuperfluidWrappedTokenBalances(
           [owner.address],
