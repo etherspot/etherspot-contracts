4898419a7bad7a6162eb74325e84881b819593cf
Sulpiride <sobirovutkir@gmail.com>
2022-06-22 19:08:33 +0500

    refactor

--- src/bridges/facets/OwnershipFacet.sol
+++ src/bridges/facets/OwnershipFacet.sol
@@ -5,12 +5,12 @@ import { LibDiamond } from "../libs/LibDiamond.sol";
 import { IERC173 } from "../interfaces/IERC173.sol";
 
 contract OwnershipFacet is IERC173 {
-    function transferOwnership(address _newOwner) external override {
-        LibDiamond.enforceIsContractOwner();
-        LibDiamond.setContractOwner(_newOwner);
-    }
+  function transferOwnership(address _newOwner) external override {
+    LibDiamond.enforceIsContractOwner();
+    LibDiamond.setContractOwner(_newOwner);
+  }
 
-    function owner() external view override returns (address owner_) {
-        owner_ = LibDiamond.contractOwner();
-    }
+  function owner() external view override returns (address owner_) {
+    owner_ = LibDiamond.contractOwner();
+  }
 }
