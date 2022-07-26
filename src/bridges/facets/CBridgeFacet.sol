b9acab7b941036e34369c36f7bba3666574ecc3a
Luke Wickens <luke@pillarproject.io>
2022-07-26 11:24:42 +0100

    Modified ReentrancyGuard contract and moved
    - Changed the contract name to DiamondReentrancyGuard.sol
    - Moved the contract into common/helpers and updated the path in the facet
    - Amended the NAMESPACE constant to something that better reflects the code

--- src/bridges/facets/CBridgeFacet.sol
+++ src/bridges/facets/CBridgeFacet.sol
@@ -9,7 +9,7 @@ pragma solidity 0.8.4;
 import {ICBridge} from "../interfaces/ICBridge.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import {ReentrancyGuard} from "../helpers/ReentrancyGuard.sol";
+import {ReentrancyGuard} from "../../common/helpers/DiamondReentrancyGuard.sol";
 import {CannotBridgeToSameNetwork, InvalidAmount, InvalidConfig, TokenAddressIsZero, ZeroAddressProvided} from "../errors/GenericErrors.sol";
 import {CBSlippageTooLow} from "../errors/CBridgeErrors.sol";
 import {LibDiamond} from "../libs/LibDiamond.sol";
