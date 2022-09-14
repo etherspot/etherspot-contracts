1ca5a831c898966c3154f46a7ce5952818456ba0
zincoshine <psramanuj@gmail.com>
2022-09-14 08:23:35 +0100

    Deployed on Arbitrum Nova and removed deployments from .gitignore

--- dist/deploy/204_deploy_cbridge_facet.js
+++ dist/deploy/204_deploy_cbridge_facet.js
@@ -1,15 +1,15 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const ethers_1 = require("ethers");
 const hardhat_1 = require("hardhat");
 const diamond_1 = require("../utils/diamond");
-const cBridge_1 = __importDefault(require("../config/cBridge"));
+const cBridge_1 = require("../config/cBridge");
 const func = async function (hre) {
-    const { deployments: { deploy }, getNamedAccounts, ethers, } = hre;
+    const { deployments: { deploy, log }, getNamedAccounts, ethers, } = hre;
     const { from } = await getNamedAccounts();
+    if (!cBridge_1.CBridgeConfig[hardhat_1.network.name]) {
+        return log("No cbridge config for this network available: " + hardhat_1.network.name);
+    }
     let bridgeAddr = "0xc578cbaf5a411dfa9f0d227f97dadaa4074ad062";
     await deploy("CBridgeFacet", {
         from,
@@ -20,11 +20,8 @@ const func = async function (hre) {
     const diamond = await ethers.getContract("Diamond");
     const ABI = ["function cbInitialize(address)"];
     const iface = new ethers_1.utils.Interface(ABI);
-    if (!cBridge_1.default[hardhat_1.network.name]) {
-        return;
-    }
-    if (cBridge_1.default[hardhat_1.network.name].cBridge != "") {
-        bridgeAddr = cBridge_1.default[hardhat_1.network.name].cBridge;
+    if (cBridge_1.CBridgeConfig[hardhat_1.network.name].cBridge != "") {
+        bridgeAddr = cBridge_1.CBridgeConfig[hardhat_1.network.name].cBridge;
     }
     const initData = iface.encodeFunctionData("cbInitialize", [bridgeAddr]);
     await (0, diamond_1.addOrReplaceFacets)([cBridgeFacet], diamond.address, cBridgeFacet.address, initData);
