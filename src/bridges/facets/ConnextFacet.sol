c4b18ee2ba617c6b8ccf24be9db7ce966feee0f6
lbw33 <71776468+lbw33@users.noreply.github.com>
2023-06-20 14:31:35 +0100

    Removed connext/nxtp-contracts package as causing deployment issues with artifact clashing. Created IConnext and updated contract. Amended deployment script issue.

--- src/bridges/facets/ConnextFacet.sol
+++ src/bridges/facets/ConnextFacet.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-import {IConnext} from "@connext/nxtp-contracts/contracts/core/connext/interfaces/IConnext.sol";
+import {IConnext} from "../interfaces/IConnext.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {IWETH} from "../interfaces/IWETH.sol";
@@ -9,9 +9,7 @@ import {LibDiamond} from "../libs/LibDiamond.sol";
 
 /**
  * @title Connext Integration
- *
  * @notice Contract which provides bridging functionality through Connext
- *
  */
 contract ConnextFacet {
     using SafeERC20 for IERC20;
